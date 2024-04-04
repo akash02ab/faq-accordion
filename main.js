eba2694199a0a31bd74951577cef33109618d519
akash02ab <akash02.ab@gmail.com>
2024-04-04 16:12:07 +0530

    solution added

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const tabs = document.querySelectorAll(".tab");
 tabs.forEach((tab, index) => {
 	tab.addEventListener("click", (event) => {
 		const currentElement = event.target;
-		if (!currentElement.classList.contains("heading")) {
+		const classNames = ["heading", "open", "close"];
+		const isTabClicked = classNames.some((className) => currentElement.classList.contains(className));
+		if (!isTabClicked) {
 			return;
 		}
 		if (tab.classList.contains("active")) {
